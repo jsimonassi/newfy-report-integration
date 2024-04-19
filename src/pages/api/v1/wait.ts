044d1ee0e47dce9c4e1e3bcf21bf037ce20d9a45
jsimonassi <jsimonassi@id.uff.br>
2024-04-19 15:27:01 -0300

    Change timeout limit

--- src/pages/api/v1/wait.ts
+++ src/pages/api/v1/wait.ts
@@ -1,17 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const MAX_VERCEL_WAITING_TIME = 7000;
+
 const wait = (req: NextApiRequest, res: NextApiResponse) => {
 
     const startWaiting = async () => {
-        const waitingTimeMs = req.body?.waitingTimeMs ?? 10000;
-        const startTime = new Date().toLocaleString();
+        const waitingTimeMs = req.body?.waitingTimeMs && req.body?.waitingTimeMs < MAX_VERCEL_WAITING_TIME ? 
+        req.body?.waitingTimeMs :
+        MAX_VERCEL_WAITING_TIME;
+        const startTime = new Date().toUTCString();
         
         setTimeout(() => {
             return res.status(200).json(
                 { 
                     message: "Espera realizada!",
+                    tempoDeEspera: waitingTimeMs,
                     startTime: startTime,
-                    endTime: new Date().toLocaleString(),
+                    endTime: new Date().toUTCString(),
                     tip: "Consulte " + req.headers.host + " para mais informações."
                 }
             );
